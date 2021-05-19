c3487c0cab2c89c3c0776fc6234e4efb5ce250a1
Sherlock1804 <dotmex2021@gmail.com>
2021-05-18 21:32:24 -0500

    bibliotecas por utilizar

--- catkin_ws/src/dotMEX-CAR_team/scripts/TMR_02_final.py
+++ catkin_ws/src/dotMEX-CAR_team/scripts/TMR_02_final.py
@@ -1,10 +1,9 @@
 #! /usr/bin/env python
-# Nodo para la prueba: Navegacion SIN obstaculos
-# Equipo: dotMEX-CAR 2021
 import cv2
 import rospy
 import numpy as np
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 from std_msgs.msg import Int16
-
+from sensor_msgs.msg import LaserScan, Imu
+	 						
