d505254f2ee106420ac1f99e69fd23169333c266
IronMakersOficial <iron.makers.oficial@gmail.com>
2021-05-19 15:10:09 -0500

    TMR

---
+++ catkin_ws/src/AVIM_folder/control/src/Master.cpp
@@ -0,0 +1,400 @@
+#include<iostream>
+#include<ros/ros.h>
+#include <std_msgs/String.h>
+#include <std_msgs/Int16.h>
+#include <std_msgs/UInt8.h>
+#include<math.h>
+#include "object_detection/points_objects.h"
+#include<string>
+#include <vector>
+#include<map>
+#include <chrono>
+#include <thread>
+
+int LANE_DRIVING = 0;
+int FOLLOWING = 1;
+int MOVING_LEFT = 2;
+int PASSING = 3;
+int MOVING_RIGHT = 4;
+int MOVING_RIGHT_LANE = 5;
+
+static std::map<int, std::string>task_names{
+     { LANE_DRIVING,"Lane driving"},
+     { FOLLOWING,"Following"},
+     { MOVING_LEFT, "Moving to left lane"},
+     { PASSING, "Passing obstacle" },
+     { MOVING_RIGHT, "Returning right lane"},
+     { MOVING_RIGHT_LANE , "Returning right lane to lane"}
+};
+
+
+class Task{
+    public:
+        std::string name;
+        int ID;
+
+        Task(int task_identifier){
+            this->ID = task_identifier;
+            this->name = task_names[this->ID];
+        }
+};
+
+
+
+class Master{
+    private:
+        ros::NodeHandle nh_;
+        ros::Subscriber distance_center;
+        ros::Subscriber on_off_sub;
+        ros::Subscriber object_detection;
+        ros::Publisher angle_pub;
+        ros::Publisher speed_pub;
+
+        std_msgs::Int16 angle_message;
+        std_msgs::Int16 speed_message;
+
+        std::vector<geometry_msgs::Point> found_objects;
+        int num_found_objects;
+        int dist_now;
+        int angle_now;
+        int angle_last;
+        float kp_angle;
+        float kd_angle;
+        float error_angle;
+        int u_angle;
+        int angle_pd;
+        int speed_pid;
+        float kp_speed;
+        int u_speed;
+        bool passing_enabled;
+        int time_long;
+        std::vector<Task> task_pile; 
+        Task *last_task;
+        int count;
+        int count_pass;
+        int max_waiting_time;
+        float dist_to_keep;
+        int vel_decreasing_factor;
+        std::chrono::steady_clock::time_point start;
+        std::chrono::steady_clock::time_point end;
+   
+    public:
+        //Constructor
+        Master(Task task, bool PASSING_ENABLED,int MAX_WAIT_TIME, float DIST_TO_KEEP){
+            //ros::Subscriber angle_now = nh.subscribe("/angle_line_now",1,angle_nowCallback);
+            distance_center = nh_.subscribe("/distance_center_line",1, &Master::dist_center_clbk, this);
+            object_detection = nh_.subscribe("/objects_points", 1, &Master::object_detec_clbk, this);
+            angle_pub = nh_.advertise<std_msgs::Int16>("/AutoModelMini/manual_control/steering",1000);
+            speed_pub = nh_.advertise<std_msgs::Int16>("/AutoModelMini/manual_control/speed",1000);
+            num_found_objects = 0;
+            dist_now = 0;
+            angle_now = 0;
+            angle_last = 0;
+            kp_angle = 0.825;//1.025
+            kd_angle = 0.0297;
+            error_angle = 0.0;
+            u_angle = 0;
+            speed_pid = 0;
+            kp_speed = 1.2645;
+            u_speed = 0;
+            count = 0;
+            count_pass = 0;
+            vel_decreasing_factor = -15;
+            dist_to_keep = DIST_TO_KEEP;
+            max_waiting_time = MAX_WAIT_TIME; 
+            passing_enabled = PASSING_ENABLED;
+            this->add_task(task);
+        }
+
+
+        void dist_center_clbk(const std_msgs::Int16& dis_now_center){
+            dist_now = static_cast<int>(dis_now_center.data);
+            this->run();
+            this->publish_policies();
+            //ROS_INFO("Dist center %d", dist_now);
+        }
+
+        void object_detec_clbk(const object_detection::points_objects::ConstPtr& msg) {
+            found_objects.clear();
+            std::map<float, float>points_order;
+            //ROS_INFO("%d", msg->another_field);
+            for(auto point :  msg->points){
+                points_order.insert ( std::pair<float, float>(point.x, point.y) );
+            //ROS_INFO("first point: x=%.2f, y=%.2f", point.x, point.y);
+            } 
+            auto first_point = points_order.begin();
+            geometry_msgs::Point point_msg;
+            point_msg.x = first_point->first;
+            point_msg.y = first_point->second;
+            found_objects.push_back(point_msg);
+            num_found_objects = found_objects.size();
+        }
+
+        void add_task(Task task){
+            this->task_pile.push_back(task);
+        }
+
+        void remove_task(){
+            if (this->task_pile.size() > 1)
+                this->task_pile.pop_back();
+        }
+        Task get_current_task(void){
+            return this->task_pile.back();
+        }
+
+        void task_assigner(void){
+            // Get the current task   
+            Task current_task = get_current_task();
+            // Checks the number of detected obstacles.
+            if (this->num_found_objects > 0){
+                                
+                //Checks each obstacle info
+                for (auto obstacle : this->found_objects){
+                    // Obstacle in front detected while driving
+                    if ((obstacle.y > 70.0) && (obstacle.y < 110.0) && (obstacle.x < 119.0)){
+
+                       if (current_task.ID == LANE_DRIVING){
+                            // Adds following routine
+                            this->add_task(Task(FOLLOWING));
+                            last_task =  new Task(FOLLOWING);
+                            break;
+                       }
+                    }
+                     if ((current_task.ID == FOLLOWING) && (this->count > this->max_waiting_time)){
+                            //Resets count
+                            count_pass += 1;
+                            this->count = 0;
+                            this->add_task(Task(MOVING_RIGHT_LANE));
+                            this->add_task(Task(MOVING_RIGHT));
+                            this->add_task(Task(PASSING));
+                            this->add_task(Task(MOVING_LEFT));
+                            break;
+                        }     
+                }
+            }
+        }
+        
+        void task_solver(void){
+            Task current_task = this->get_current_task();
+            ROS_INFO_STREAM("[Current task]: " <<current_task.name);
+            if (current_task.ID == LANE_DRIVING){
+                on_lane();
+            }
+            else if (current_task.ID == FOLLOWING){
+                if (count_pass > 5){
+                    count_pass = 0;
+                }
+                // If no obstacles detected
+                if (this->num_found_objects == 0){
+                    on_lane();
+                    this->remove_task();
+                }
+                else{
+                    for (auto obstacle : this->found_objects){
+                        // No obstacle in front, finish task
+                        if (obstacle.y >= 225.0 && obstacle.y <= 315.0 || last_task->ID == MOVING_RIGHT_LANE){
+                            on_lane();
+                            this->count = 0;
+                            this->remove_task();
+                            break;
+                        }
+                        else if(obstacle.x > (this->dist_to_keep + 10.0)){
+                            this->count = 0;
+                            if(count_pass == 3 || count_pass == 4){
+                                on_lane_front_object_cross(obstacle.x);
+                            }
+                            else{
+                                on_lane_front_object(obstacle.x);
+                            }
+                            break;
+                        }
+                        else if (obstacle.x < (this->dist_to_keep - 10)){
+                            this->count = 0;
+                            if(count_pass == 3 || count_pass == 4){
+                               on_lane_front_object_cross(obstacle.x);
+                            }
+                            else{
+                                on_lane_front_object(obstacle.x);
+                            }
+                            break;
+                        }
+                        else{
+                            // Counting
+                            if (this->passing_enabled == true){
+                                this->count += 1;
+                            }
+                            std::this_thread::sleep_for(std::chrono::milliseconds(500));
+                            on_lane_stop();
+                            break;    
+                        }
+                    }
+                }
+            }
+            else if (current_task.ID == MOVING_LEFT){
+                for (auto obstacle : this->found_objects){
+                    if ((obstacle.y >= 60.0) && (obstacle.y <= 135.0)){
+                        if(count_pass == 5){
+                            speed_pid = -250;
+                            angle_pd = 170;
+                        }
+                        else{
+                            speed_pid = -250;
+                            angle_pd = 160;
+                        }
+                        break;
+                    }
+                    else if((obstacle.y >= 0.0 && obstacle.y < 60.0) || (obstacle.y >= 360.0 && obstacle.y <= 315.0)){
+                        on_lane();
+                        this->remove_task();
+                        break;
+                    }
+                }
+            }
+            else if (current_task.ID ==  PASSING){
+                for (auto obstacle : this->found_objects){
+                    if ((obstacle.y >= 270.0) && (obstacle.y <= 345.0)){
+                        speed_pid = -300;
+                        angle_pd = 20;
+                        this->remove_task();
+                        start =  std::chrono::steady_clock::now();
+                        break;
+                    }
+                    else {
+                        if(count_pass > 3){
+                            speed_pid = -200;
+                            angle_pd = 65;
+                        }
+                        else{
+                            on_lane();
+                        }
+                    }
+                }
+            }
+            else if (current_task.ID ==   MOVING_RIGHT){
+                time_long = 2250;
+                if (count_pass == 5) {
+                    time_long = 4100;
+                }
+                else if(count_pass == 3){
+                    time_long = 1700;
+                }
+                end =  std::chrono::steady_clock::now();
+                    if ( (std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() > time_long ) ){
+                        on_lane_right();
+                        this->remove_task();
+                        start =  std::chrono::steady_clock::now();
+                    }
+                    else{
+                        speed_pid = -200;
+                        angle_pd = 20;
+                    }
+            }
+            else if (current_task.ID ==   MOVING_RIGHT_LANE){
+                //time_long = 20000;
+                /*
+                if(count_pass == 3){
+                    time_long = 25000;
+                }
+                */
+                 end =  std::chrono::steady_clock::now();
+                    if ( std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() >  12500){
+                        on_lane_right();
+                        this->remove_task();
+                    }
+                    else{
+                        on_lane_right();
+                    }
+            }
+            last_task =  new Task(current_task.ID);
+        }
+
+        void run(void){
+            this->task_assigner();
+            this->task_solver();
+        }
+
+        void on_lane(void){
+            u_angle = static_cast<int>(kp_angle*static_cast<float>(dist_now) + kd_angle*static_cast<float>(dist_now - angle_last));
+            angle_pd = 90 + u_angle;
+            if(angle_pd<= 45)
+	            angle_pd = 45;
+            else if(angle_pd >= 135)
+	            angle_pd = 135;
+            u_speed = static_cast<int>(kp_speed * dist_now);
+            speed_pid = - 535 + abs(u_speed);
+            //speed_pid = -200;
+            if(speed_pid < - 535)
+	            speed_pid = - 535;
+            else if(speed_pid > 0)
+	            speed_pid = 0;
+            angle_last = angle_pd;
+        }
+
+        void on_lane_right(void){
+            u_angle = static_cast<int>(kp_angle*static_cast<float>(dist_now) + kd_angle*static_cast<float>(dist_now - angle_last));
+            angle_pd = 90 + u_angle;
+            if(angle_pd<= 0)
+	            angle_pd = 0;
+            else if(angle_pd >= 180)
+	            angle_pd = 180;
+            speed_pid = - 150;
+            angle_last = angle_pd;
+        }
+
+         void on_lane_front_object_cross(float obstacle_dist){
+            speed_pid =  decrement_speed(obstacle_dist);
+            if(speed_pid < - 535)
+	            speed_pid = - 535;
+            else if(speed_pid >= 250)
+	            speed_pid = 250;
+            angle_pd = 90;
+         }
+
+        void on_lane_front_object(float obstacle_dist){
+            u_angle = static_cast<int>(kp_angle * static_cast<float>(dist_now) + kd_angle * static_cast<float>(dist_now - angle_last));
+            angle_pd = 90 + u_angle;
+            if(angle_pd<= 45)
+	            angle_pd = 45;
+            else if(angle_pd >= 135)
+	            angle_pd = 135;
+            speed_pid =  decrement_speed(obstacle_dist);
+            //speed_pid = -200;
+            if(speed_pid < - 535)
+	            speed_pid = - 535;
+            else if(speed_pid >= 250)
+	            speed_pid = 250;
+            angle_last = angle_pd;
+        }
+        
+        int decrement_speed(float obstacle_dist){
+            int distance_diff, dist_to_obstacle;
+            dist_to_obstacle = static_cast<int>(obstacle_dist);
+            distance_diff = dist_to_obstacle  - dist_to_keep;
+            return vel_decreasing_factor * distance_diff;
+        }
+
+        void on_lane_stop(void){
+            u_angle = static_cast<int>(kp_angle*static_cast<float>(dist_now) + kd_angle*static_cast<float>(dist_now - angle_last));
+            angle_pd = 90 + u_angle;
+            if(angle_pd<= 45)
+	            angle_pd = 45;
+            else if(angle_pd >= 135)
+	            angle_pd = 135;
+            speed_pid = 0;
+            angle_last = angle_pd;
+        }
+        void publish_policies(){
+            angle_message.data = angle_pd;
+            angle_pub.publish(angle_message);
+            speed_message.data = speed_pid;
+            speed_pub.publish(speed_message);
+        }
+};
+
+
+int main(int argc, char **argv){
+    ros::init(argc, argv, "Master");
+    Master *control = new  Master(Task(LANE_DRIVING), true, 5, 115);
+    ros::spin();
+    return 0;
+}
---
+++ catkin_ws/src/AVIM_folder/object_detection_parking/src/object_detection_parking.cpp
@@ -0,0 +1,372 @@
+#include <ros/ros.h>
+#include <signal.h>
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+#include <sensor_msgs/image_encodings.h>
+#include <ros/console.h>
+#include <cmath>
+#include <vector>
+#include<map>
+#include <iostream>
+#include <sensor_msgs/LaserScan.h>
+#include <std_msgs/String.h>
+#include <std_msgs/Int16.h>
+#include <std_msgs/Float32.h>
+#include "object_detection/points_objects.h"
+#include <math.h>
+
+#define UNCLASSIFIED -1
+#define CORE_POINT 1
+#define BORDER_POINT 2
+#define NOISE -2
+#define SUCCESS 0
+#define FAILURE -3
+
+
+
+static const std::string OPENCV_WINDOW = "OBJECT DETECTION";
+typedef std::vector<cv::Point2f> vector_point;
+typedef std::vector<vector_point> obstacles;
+typedef std::vector<cv::Point> Cluster;
+// Global Parameters
+bool DEBUG = true;
+float RANGE = 0.44;
+float RANGE_MIN = 0.18;
+float INITIAL_RANGE_ANGLE = 0.0;
+float END_RANGE_ANGLE = 360.0;
+
+
+class PointC
+{
+    public:
+        float x, y;  // X, Y position
+        int clusterID;  // clustered ID
+        PointC(float x, float y){
+            this->x = x;
+            this->y = y;
+            this->clusterID = UNCLASSIFIED;
+        }
+};
+
+
+class DBSCAN {
+public: 
+    std::vector<PointC> m_points;
+    DBSCAN(unsigned int minPts, float eps,const std::vector<cv::Point> &points_){
+        m_minPoints = minPts;
+        m_epsilon = eps;
+        for (auto point : points_){
+            m_points.push_back({(float)(point.x),(float)(point.y)}); 
+        }
+        m_pointSize =  m_points.size();
+    }
+    ~DBSCAN(){}
+
+    int run();
+    std:: vector<int> calculateCluster(PointC point);
+    int expandCluster(PointC point, int clusterID);
+    inline double calculateDistance(PointC pointCore, PointC pointTarget);
+    int getTotalPointSize() {return m_pointSize;}
+    int getMinimumClusterSize() {return m_minPoints;}
+    int getEpsilonSize() {return m_epsilon;}
+    void getCluster(std::map<int, std::vector<cv::Point>>&);
+    
+private:
+    unsigned int m_pointSize;
+    unsigned int m_minPoints;
+    float m_epsilon;
+};
+
+int DBSCAN::run()
+{
+    int clusterID = 0;
+    std::vector<PointC>::iterator iter;
+    for(iter = m_points.begin(); iter != m_points.end(); ++iter)
+    {
+        if ( iter->clusterID == UNCLASSIFIED )
+        {
+            if ( expandCluster(*iter, clusterID) != FAILURE )
+            {
+                clusterID += 1;
+            }
+        }
+    }
+
+    return 0;
+}
+
+int DBSCAN::expandCluster(PointC point, int clusterID)
+{    
+    std::vector<int> clusterSeeds = calculateCluster(point);
+
+    if ( clusterSeeds.size() < m_minPoints )
+    {
+        point.clusterID = NOISE;
+        return FAILURE;
+    }
+    else
+    {
+        int index = 0, indexCorePoint = 0;
+        std::vector<int>::iterator iterSeeds;
+        for( iterSeeds = clusterSeeds.begin(); iterSeeds != clusterSeeds.end(); ++iterSeeds)
+        {
+            m_points.at(*iterSeeds).clusterID = clusterID;
+            if (m_points.at(*iterSeeds).x == point.x && m_points.at(*iterSeeds).y == point.y)
+            {
+                indexCorePoint = index;
+            }
+            ++index;
+        }
+        clusterSeeds.erase(clusterSeeds.begin()+indexCorePoint);
+
+        for(std::vector<int>::size_type i = 0, n = clusterSeeds.size(); i < n; ++i )
+        {
+            std::vector<int> clusterNeighors = calculateCluster(m_points.at(clusterSeeds[i]));
+
+            if ( clusterNeighors.size() >= m_minPoints )
+            {
+                std::vector<int>::iterator iterNeighors;
+                for ( iterNeighors = clusterNeighors.begin(); iterNeighors != clusterNeighors.end(); ++iterNeighors )
+                {
+                    if ( m_points.at(*iterNeighors).clusterID == UNCLASSIFIED || m_points.at(*iterNeighors).clusterID == NOISE )
+                    {
+                        if ( m_points.at(*iterNeighors).clusterID == UNCLASSIFIED )
+                        {
+                            clusterSeeds.push_back(*iterNeighors);
+                            n = clusterSeeds.size();
+                        }
+                        m_points.at(*iterNeighors).clusterID = clusterID;
+                    }
+                }
+            }
+        }
+
+        return SUCCESS;
+    }
+}
+
+std::vector<int> DBSCAN::calculateCluster(PointC point)
+{
+    int index = 0;
+    std::vector<PointC>::iterator iter;
+    std::vector<int> clusterIndex;
+    for( iter = m_points.begin(); iter != m_points.end(); ++iter)
+    {
+        if ( calculateDistance(point, *iter) <= m_epsilon )
+        {
+            clusterIndex.push_back(index);
+        }
+        index++;
+    }
+    return clusterIndex;
+}
+
+inline double DBSCAN::calculateDistance(PointC pointCore, PointC pointTarget )
+{
+    return sqrt((pointCore.x - pointTarget.x)*(pointCore.x - pointTarget.x)+(pointCore.y - pointTarget.y)*(pointCore.y - pointTarget.y));
+}
+
+void DBSCAN::getCluster(std::map<int, std::vector<cv::Point>> &clusters_points){
+    for (auto point: m_points){
+        clusters_points[point.clusterID].push_back(cv::Point(int(point.x), int(point.y)));
+    }
+}
+
+
+class ObjectDetection
+{
+    ros::NodeHandle nh;
+    ros::Subscriber scan_sub;
+    sensor_msgs::LaserScan newscan;
+    vector_point scan_points;
+    std::vector<cv::Point> points_;
+    int MINIMUM_POINTS;         // minimum number of cluster
+    int EPSILON;                // distance for clustering
+    std::map<int,std::vector<cv::Point>> clusters_points;
+    std::vector<cv::Point> points_centroids;
+    cv::Mat image;
+    ros::Publisher pub;
+    public:
+
+    ObjectDetection(): nh(){
+        MINIMUM_POINTS = 1;
+        EPSILON = 10.0;
+        pub = nh.advertise<object_detection::points_objects>("objects_points", 1);
+        scan_sub = nh.subscribe("/scan", 1, &ObjectDetection::laser_msg_Callback, this); 
+    }
+
+        //Destructor
+        ~ObjectDetection(){
+          	cv::destroyWindow(OPENCV_WINDOW);
+        }
+    void laser_msg_Callback( const sensor_msgs::LaserScan::ConstPtr& scan){
+        // Local variables
+        int cnt, detected_obstacles;
+        float elapsed_time, dist_norm;
+        std::vector<float> ROI;
+        std::vector<float> ranges;
+        std::vector<float> intensities;
+        vector_point scan_points, obstacle;
+        obstacles detected_obstacles_vector;
+        cv::Point2f last_point, current_point;
+
+        object_detection::points_objects points_msg;
+        points_msg.another_field = 0;
+
+        // Fill the ranges vector
+        for (float i = 0.0; i <= 360.0; i++){
+            ranges.push_back(std::numeric_limits<float>::infinity());
+            intensities.push_back(std::numeric_limits<float>::infinity());  
+        }
+
+        // Range to be considered
+        if (INITIAL_RANGE_ANGLE >= END_RANGE_ANGLE){
+            float i = INITIAL_RANGE_ANGLE;
+            while (i<360){
+                ROI.push_back(i);
+                i += 1.0;
+            }
+            i = 0.0;
+            while (i < END_RANGE_ANGLE){
+                ROI.push_back(i);
+                i += 1.0;
+            }
+        }
+        else{
+            float i = INITIAL_RANGE_ANGLE;
+            while (i < END_RANGE_ANGLE){
+                ROI.push_back(i);
+                i += 1.0;
+            }
+        }
+
+        // Takes points within a certian distance range
+        for (std::vector<float>::iterator i = ROI.begin();i != ROI.end(); i++){ 
+            if(scan->ranges[*i] <= RANGE &&  scan->ranges[*i] >= RANGE_MIN)
+            {
+                ranges[*i] = scan->ranges[*i];
+                intensities[*i] = scan->intensities[*i];
+                scan_points.push_back(
+                cv::Point2f(*i, scan->ranges[*i]));
+           }
+        }
+        image = cv::Mat::zeros( 800, 800, CV_8UC3 );
+        get_object_points(scan_points);
+        circle(image, cv::Point( 400, 400 ), 50.0, cv::Scalar( 255, 0, 0), 2);
+        circle(image, cv::Point(400 , 400),cvRound((double)4/ 2), cv::Scalar(0, 255, 0),2);
+        line(image, cv::Point(400,0), cv::Point(400,800), cv::Scalar(255, 0, 0), 2); 
+        line(image, cv::Point(0,400), cv::Point(800,400), cv::Scalar(255, 0, 0), 2); 
+        //std::cout << "Number clusters " << clusters_points.size() << std::endl;
+        
+
+        if (points_centroids.size() > 0){
+            points_msg.another_field = points_centroids.size();
+            for (auto point : points_centroids){
+                geometry_msgs::Point point_msg;
+                line(image, cv::Point(400,400), point, cv::Scalar(0, 0, 255), 2);                
+                transform_point(point, point_msg);
+                point_msg.z = 0;
+                points_msg.points.push_back(point_msg);
+                //std::cout << "x: "<< point.x << std::endl; 
+                //std::cout << "y: "<< point.y << std::endl; 
+                ROS_INFO("x: %.2f, y: %.2f", point_msg.x, point_msg.y);
+            }
+            pub.publish(points_msg);
+        }
+        
+        resize_image(image);
+        cv::resizeWindow(OPENCV_WINDOW, image.cols, image.rows);
+        imshow(OPENCV_WINDOW,image);
+        cv::waitKey(15);
+    }
+
+    void get_object_points(const vector_point& scan_points){
+        std::vector <PointC> points_dbscan;
+        points_.clear();
+        clusters_points.clear();
+        float x, y, r;
+        for (auto point : scan_points){
+            r = point.y * 100.0;
+            x = (r)*cos(static_cast<float>(180-point.x)*3.14159/180.0);
+            y = (r)*sin(static_cast<float>(180-point.x)*3.14159/180.0);
+            x = 400.0 + x;
+            x = x < 800 ? x : 800;
+            x = x >= 0 ? x : 0; 
+            y = 400.0 + y;
+            y = y < 800 ? y : 800;
+            y = y >= 0 ? y : 0; 
+            points_.push_back(cv::Point(x, y));
+        }
+        DBSCAN dbScan(MINIMUM_POINTS,EPSILON,points_);
+        dbScan.run();
+        dbScan.getCluster(clusters_points);
+        show_clusters(image, clusters_points);
+        get_centroids_objects(clusters_points);
+    }
+
+    void transform_point(cv::Point &point, geometry_msgs::Point &point_msg){
+        float adjacent_leg, opposite_leg;
+        adjacent_leg = point.y - 400; 
+        opposite_leg = point.x - 400;
+        adjacent_leg = (adjacent_leg == 0.0) ? 0.000001 : adjacent_leg;
+        if (point.x >= 0 && point.x <= 400 && point.y >= 0 && point.y <= 400){
+            point_msg.y =  atan(opposite_leg / adjacent_leg)* 57.295779;
+        }
+        else if  (point.x >= 0 && point.x <= 400 && point.y >= 400 && point.y <= 800){
+            point_msg.y =  180.0 + atan(opposite_leg / adjacent_leg)* 57.295779;
+        }
+        else if  (point.x >= 400 && point.x <= 800 && point.y >= 400 && point.y <= 800){
+            point_msg.y =  180.0 + atan(opposite_leg / adjacent_leg)* 57.295779;
+        }
+        else if  (point.x >= 400 && point.x <= 800 && point.y >= 0 && point.y <= 400){
+            point_msg.y =  360.0 + atan(opposite_leg / adjacent_leg)* 57.295779;
+        }
+        if (point_msg.y < 0){
+            point_msg.y = (-1) * point_msg.y;
+        }
+        point_msg.x = cv::norm(point - cv::Point(400,400));
+    }
+
+    void resize_image(cv::Mat &input,float alpha=0.35){
+		    cv::resize(input,input,cv::Size(input.cols*alpha,input.rows*alpha));
+	}
+
+    void show_clusters(cv::Mat &frame,const std::map<int,std::vector<cv::Point>> &clusteredPoints_){
+            int colors[3];
+            for (auto cluster : clusteredPoints_){
+                colors[0] = static_cast<int>(rand() % 255);
+                colors[1] = static_cast<int>(rand() % 255);
+                colors[2] = static_cast<int>(rand() % 255);
+                for (auto point : cluster.second){
+                    circle(frame, cv::Point(point.x , point.y),cvRound((double)4/ 2), cv::Scalar(colors[0], colors[1], colors[2]),2);
+                }
+                //std::cout << cluster.second.size()<<std::endl;
+            }
+        }
+
+    void  get_centroids_objects(const std::map<int,std::vector<cv::Point>> &clusteredPoints_){
+        points_centroids.clear();
+        int x , y, n;
+        for (auto cluster : clusteredPoints_){
+            x = y = 0;
+            n = cluster.second.size();
+            for (auto point : cluster.second){
+                x += point.x; 
+                y += point.y;
+            }
+            points_centroids.push_back(cv::Point(x / n , y / n));
+        }
+    }
+
+};
+
+
+int main(int argc,char **argv)
+{
+    ros::init(argc, argv, "ObjectDetection");
+    ROS_INFO("Object detection node running...");
+    ObjectDetection *od= new  ObjectDetection;
+    ros::spin();
+    return 0;
+}
