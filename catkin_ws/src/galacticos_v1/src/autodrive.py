691ee6283e6378a80b6a360f68213375c6d2a28a
boribs <christumer@gmail.com>
2021-05-19 15:06:34 -0500

    Ignora aquellas líneas con varios puntos medios.
    - Quita todos los prints y demás formas de demostrar información

--- catkin_ws/src/galacticos_v1/src/autodrive.py
+++ catkin_ws/src/galacticos_v1/src/autodrive.py
@@ -30,7 +30,6 @@ LINEAS_Y = [
 
 def girar(grados):
     if grados:
-        rospy.loginfo('girando a ' + str(grados))
         gir_pub.publish(grados)
 
 
@@ -52,8 +51,6 @@ def avanzar(velocidad, tiempo):
 
 def procesar_imagen_1era(message):
     global binary_image, cv_image
-
-    rospy.loginfo('1era')
     bridge_object = CvBridge()
 
     try:
@@ -67,19 +64,6 @@ def procesar_imagen_1era(message):
         ]
 
         pm = determina_puntos_medios(lineas_y, True)
-        for i in range(len(lineas_y)):
-            cv2.line(cv_image, (0, lineas_y[i]), (640, lineas_y[i]), (0, 255, 0), 1)
-            cv2.line(
-                cv_image,
-                (pm[i], lineas_y[i] - 3),
-                (pm[i], lineas_y[i] + 3),
-                (0, 150, 200),
-                1
-            )
-
-        cv2.imwrite('img.png', cv_image)
-        cv2.waitKey(3)
-
         grados = None
 
         for punto in pm:
@@ -90,7 +74,6 @@ def procesar_imagen_1era(message):
         if not grados:
             raise Exception('no se encontro ningun punto medio')
 
-        rospy.loginfo('acomodar ' + str(grados) + ' grados')
         dir = 90 + grados
 
         girar(dir)
@@ -125,7 +108,6 @@ def determina_puntos_medios(lineas_y=LINEAS_Y, inverso=False):
                 if not inicio:
                     inicio = x
                 fin = x
-
             else:
                 if inicio and fin:
                     blancos.append((inicio + fin) // 2)
@@ -135,7 +117,7 @@ def determina_puntos_medios(lineas_y=LINEAS_Y, inverso=False):
             blancos.append((inicio + fin) // 2)
             inicio = fin = None
 
-        if len(blancos) > 1:
+        if len(blancos) > 1 and len(blancos) < 3:
             puntos_medios.append((blancos[0] + blancos[1]) // 2)
         else:
             puntos_medios.append(-1)
@@ -152,39 +134,13 @@ def procesar_imagen(message):
         gray_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
         _, binary_image = cv2.threshold(gray_image, 200, 255, cv2.THRESH_BINARY)
 
-        puntos_medios = determina_puntos_medios()
-        rospy.loginfo(puntos_medios)
-
-        lineas_y = LINEAS_Y
-
-        for i in range(len(lineas_y)):
-            cv2.line(cv_image, (0, lineas_y[i]), (640, lineas_y[i]), (0, 255, 0), 1)
-            cv2.line(
-                cv_image,
-                (puntos_medios[i], lineas_y[i] - 3),
-                (puntos_medios[i], lineas_y[i] + 3),
-                (0, 150, 200),
-                1
-            )
-
-        cv2.rectangle(
-            cv_image,
-            (rectangulo[0],LINEA_Y - MARGEN_LINEA),
-            (rectangulo[1],LINEA_Y + MARGEN_LINEA),
-            (255, 0, 0), 1
-        )
-
-        # cv2.imshow("binary image", binary_image)
-        cv2.imshow('RGB image', cv_image)
-        cv2.waitKey(3)
-
         dir = determina_direccion()
 
         if dir:
-        if dir > 95:
-            dir += 20
-        elif dir < 60:
-            dir -= 10
+            if dir > 95:
+                dir += 20
+            elif dir < 60:
+                dir -= 10
 
         girar(dir)
         avanzar(-125, 0.1)
@@ -199,10 +155,8 @@ def determina_direccion():
     for punto in pm:
         if punto > 0:
             grados = 90 + (rectangulo[0] - punto) / 3
-            rospy.loginfo('g: ' + str(grados))
             return grados
 
-    rospy.loginfo('aaa')
     return None
 
 
@@ -222,8 +176,6 @@ if __name__ == '__main__':
         rospy.sleep(1)
 
         procesar_imagen_1era(rospy.wait_for_message(CAM_TOPIC, Image))
-
-        rospy.loginfo('inicio')
 
         try:
             rospy.spin()
