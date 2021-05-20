199f9fe21f8f5c164bf4a5cc7d1773015b96dfda
joaccomi <60494029+joaccomi@users.noreply.github.com>
2021-05-20 14:34:29 -0500

    Update movimientoCarro2.py

--- catkin_ws/src/LaSalleMX_team/src/movimientoCarro2.py
+++ catkin_ws/src/LaSalleMX_team/src/movimientoCarro2.py
@@ -65,10 +65,10 @@ def cambiospertinentes(desicionObject,vel):
         ##########################################################
         print("esquivando")
         for i in range(200):
-            publisher(390,-120)
+            publisher(390,120)
             print("alteramos")
         for i in range(250):
-            publisher(-50,-120)
+            publisher(-50,120)
         #switch=1
         
         
